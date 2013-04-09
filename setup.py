9020a24fff8308c1fc836a12aa65579a581fb3e8
Adam Hinz <hinz.adam@gmail.com>
2013-04-08 22:45:14 -0400

    A distribute setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='eco.py',
+    version='0.0.1',
+    description='Calculate eco benefits for urban trees',
+    long_description=readme,
+    author='Adam Hinz',
+    author_email='hinz.adam@gmail.com',
+    url='https://github.com/azavea/eco.py',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
